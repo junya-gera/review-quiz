712306e917da78dcd5d533ecb022673b22d0fb90
junya-gera <aa106921@gmail.com>
2020-06-05 14:32:55 +0900

    スタートを押したらクイズが表示される
    GameクラスからQuizクラスのquizSetプロパティを呼び出した

--- review.js
+++ review.js
@@ -4,10 +4,10 @@
 
   class Quiz {
     constructor(){
-      const quizSet = this.shuffle([
+      this.quizSet = this.shuffle([
         {q: 'What is A?', c: ['A0','A1','A2']},
         {q: 'What is B?', c: ['B0','B1','B2']},
-        {q: 'What is C?', c: ['C0','C1','C2']}    
+        {q: 'What is C?', c: ['C0','C1','C2']}
       ]);
     }
 
@@ -29,8 +29,20 @@
       const start = document.getElementById('start');
       start.addEventListener('click', () => {
         start.classList.add('disabled');
-      });  
+        this.currentNum = 0;
+        this.question = document.getElementById('question');
+        this.quiz = new Quiz();
+        this.setQuiz();  
+      });
+
+
     }
+
+
+    setQuiz() {
+      this.question.textContent = this.quiz.quizSet[this.currentNum].q;
+    }
+  
   
   }
 
