5a40f8b158d088f9ef6d9ae61175ce64fb6aae77
junya-gera <aa106921@gmail.com>
2020-06-07 22:20:14 +0900

    コメントで補足を追加

--- review.js
+++ review.js
@@ -11,6 +11,7 @@
       ]);
     }
 
+    // 選択肢の順番をリセットするメソッド
     shuffle(arr){
       // フィッシャー・イェーツのシャッフルを使って選択肢をシャッフルする
       // i = ランダムに選ぶ範囲の終点
@@ -31,6 +32,7 @@
         start.classList.add('disabled');
 
         this.choices = document.getElementById('choices');
+        // 現在の問題の番号
         this.currentNum = 0;
         this.question = document.getElementById('question');
         this.quiz = new Quiz();
@@ -42,6 +44,7 @@
 
     setQuiz() {
       this.question.textContent = this.quiz.quizSet[this.currentNum].q;
+      // シャッフル後の選択肢
       const shuffledChoices = this.quiz.shuffle([...this.quiz.quizSet[this.currentNum].c]);
       shuffledChoices.forEach(choice => {
         const li = document.createElement('li');
